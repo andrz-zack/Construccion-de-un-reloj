cdb85e03db90c3425eb702b2909cc274a498e378
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-15 01:23:01 -0500

    Agregando scripts en main.js

---
+++ main/main.js
@@ -0,0 +1,8 @@
+
+let ID = document.getElementById.bind(document);
+
+this.onload = function(){
+    let sec, min, hr, clock, secondHand, minuteHand, hourHand, secDeg, minDeg, hrDeg, radius, start, i, spanDeg, top, right, clockIsPaused; 
+
+    //cargando el reloj
+}
