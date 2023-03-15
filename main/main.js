533764e136172fdb522a2b03403b9fc1990c2bb5
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-15 01:37:26 -0500

    Agregando la function loadClock

--- main/main.js
+++ main/main.js
@@ -5,4 +5,27 @@ this.onload = function(){
     let sec, min, hr, clock, secondHand, minuteHand, hourHand, secDeg, minDeg, hrDeg, radius, start, i, spanDeg, top, right, clockIsPaused; 
 
     //cargando el reloj
+    function loadClock(){
+        sec = new Date().getSeconds() + 2;
+        min = new Date().getMinutes();
+        hr = new Date().getHours();
+        clock = ID("clock-wrapper");
+        secondHand = ID("clock-second");
+        minuteHand = ID("clock-minute");
+        hourHand = ID("clock-hour");
+
+        secDeg = (sec * 6);
+        minDeg = (min + (sec / 60)) * 6;
+        hrDeg = ((hr - 12) * 30) + ((min / 60) * 30);
+}
+
+document.addEventListener("visibilitychange", loadClock);
+loadClock();
+
+radius = ((clock.offsetWidth - 20) / 2);
+
+/* comenzando con el reloj */
+
+
+
 }
